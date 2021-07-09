8c83655b20f053ddee4675bdeeeb30d7a107925f
duplin anton <duplin.anton@gmail.com>
2021-07-09 18:05:42 +0300

    Initial Commit

---
+++ slidersApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  slidersApp
+//
+//  Created by Anton Duplin on 9/7/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
