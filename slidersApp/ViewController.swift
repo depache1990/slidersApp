40b6b9fcf7e058aec6db0e446748db8bcbcf3b9b
duplin anton <duplin.anton@gmail.com>
2021-07-09 23:06:52 +0300

    add complete final version

--- slidersApp/ViewController.swift
+++ slidersApp/ViewController.swift
@@ -6,15 +6,54 @@
 //
 
 import UIKit
-
 class ViewController: UIViewController {
-
+    // mainLabel
+    @IBOutlet weak var mainLabel: UIView!
+    
+    
+    // LabelsColors
+    @IBOutlet weak var redLabel: UILabel!
+    @IBOutlet weak var greenLabel: UILabel!
+    @IBOutlet weak var blueLabel: UILabel!
+    
+    // LabelsNumberValue
+    @IBOutlet weak var redValueLabel: UILabel!
+    @IBOutlet weak var greenValueLabel: UILabel!
+    @IBOutlet weak var blueValueLabel: UILabel!
+    
+    
+    // Sliders
+    @IBOutlet weak var sliderRed: UISlider!
+    
+    @IBOutlet weak var sliderGreen: UISlider!
+    @IBOutlet weak var sliderBlue: UISlider!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-       
-        
+        sliderGreen.value = 1
+        sliderGreen.minimumValue = 0
+        sliderGreen.maximumValue = 1
+       }
+        func changeRgbColor() {
+            mainLabel.backgroundColor = UIColor(red: CGFloat(sliderRed.value), green: CGFloat(sliderGreen.value), blue: CGFloat(sliderBlue.value), alpha: 1)
+        }
+  
+
+    @IBAction func sliderActionRed() {
+        changeRgbColor()
+        redValueLabel.text = String(sliderRed.value)
     }
-
-
+    
+    @IBAction func sliderActionGreen() {
+        changeRgbColor()
+        greenValueLabel.text = String(sliderGreen.value)
+    }
+    
+    @IBAction func sliderActionBlue() {
+        changeRgbColor()
+        blueValueLabel.text = String(sliderBlue.value)
+    }
+    
+    
 }
 
